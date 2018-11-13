cf507ca20cfe4f291dda0268a9a0e879163e5c3c
Mitchell Scott Sandre <msandre@edu.uwaterloo.ca>
2018-11-12 21:49:50 -0500

    Working on: getting Google Sign in working. Think I need to deploy

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
 
   ngOnInit() {
+  }
+
+  loginWithGoogle() {
+    this.afAuth.auth.signInWithRedirect(new auth.GoogleAuthProvider());
   }
 
 }
